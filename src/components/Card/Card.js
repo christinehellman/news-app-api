ccbde54ddf74b2f2f8cccd3c00ba741fc29fb070
Christine Hellman <christinehellman@Christines-iMac-Pro.local>
2019-04-09 13:48:19 -0400

    jsx syntax

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = (props) => {
   const imageStyle = {
     background: 'lightgray',
-    height: 100%;
+    height: '100%'
   }
   return (
     <div className="card">
